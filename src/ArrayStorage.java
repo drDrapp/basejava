e4d97e16d4964766f7fbfd03432eba29537666a9
drDrapp <born--in--ussr@mail.ru>
2022-11-02 17:15:12 +0300

    Класс ArrayStorage - Реализовал методы: save, get, delete, clear, getAll, size.
    Changes to be committed:
      modified:   src/ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,58 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+
+    int CountResume = 0;
+
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < CountResume; i++) {
+            storage[i] = null;
+        }
+        CountResume = 0;
     }
 
     void save(Resume r) {
+        if (storage.length == CountResume) {
+            return;
+        }
+        storage[CountResume] = r;
+        CountResume++;
     }
 
     Resume get(String uuid) {
-        return null;
+        int Index = 0;
+        while (true) {
+            if (Index == CountResume) {
+                return null;
+            }
+            if (storage[Index].uuid.equals(uuid)) {
+                return storage[Index];
+            }
+            Index++;
+        }
     }
 
     void delete(String uuid) {
+        int Index = 0;
+        while (Index < CountResume) {
+            if (storage[Index].uuid.equals(uuid)) {
+                storage[Index] = storage[CountResume - 1];
+                storage[CountResume - 1] = null;
+                CountResume--;
+            }
+            Index++;
+        }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] NewStorage = new Resume[CountResume];
+        System.arraycopy(storage, 0, NewStorage, 0, CountResume);
+        return NewStorage;
     }
 
     int size() {
-        return 0;
+        return CountResume;
     }
 }
